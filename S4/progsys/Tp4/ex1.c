21b8690aa957e56fe8c0be25bae61e43036d14ba
1-mec <matteo.jonas3@gmail.com>
2026-03-09 16:36:39 +0100

    Tp4

--- S4/progsys/Tp4/ex1.c
+++ S4/progsys/Tp4/ex1.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
+
+int main(int argc, char * argv[]) {
+    struct stat st;
+    int tube[2];
+
+    int fd = open(argv[1], O_RDWR , 644);
+    int fdd = open(argv[0], O_RDWR , 644);
+
+    pipe(tube);
+
+    if (argc == 1){
+        printf("")
+    }
+
+    int fs = fstat(tube[0],&st );
+
+    printf("tube n°%d ,fd = %d , size = %d , mode = %d, inode = %d\n",tube[0],fs, st.st_size, st.st_mode, st.st_ino);
+
+}
